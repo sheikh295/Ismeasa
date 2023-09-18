3eff941a2ca402b6940d0dae4930730a0721e56f
sheikh295 <sheikh295@users.noreply.github.com>
2023-09-18 22:05:52 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+        "app": path.resolve(__dirname, "./src/"),
+        assets: `${path.resolve(__dirname, "./src/assets/")}`,
+        components:  `${path.resolve(__dirname, "./src/components/")}`,
+        pages: `${path.resolve(__dirname, "./src/pages/")}`,
+        routes: `${path.resolve(__dirname, "./src/routes/")}`,
+        shared: `${path.resolve(__dirname, "./src/shared/")}`,
+    }
+}
 })
