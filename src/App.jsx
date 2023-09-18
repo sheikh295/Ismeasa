f2e5605377a68f4f3119ba5905f9e4bda9528d72
sheikh295 <sheikh295@users.noreply.github.com>
2023-09-18 22:03:20 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import './index.css'
+import {Login, Signup, Onboarding, Dashboard, AllTables, Layout, Insights, Pricing} from 'pages'
+import { createBrowserRouter } from "react-router-dom";
+import {routes} from 'routes'
+import Auth from './Auth/Auth';
+
+export const router = createBrowserRouter([
+  {
+    path: routes.LOGIN,
+    element: <Login />,
+  },
+  {
+    path: routes.SIGNUP,
+    element: <Signup />
+  },
+  {
+    path: routes.ONBOARDING,
+    element: <Auth><Onboarding /></Auth>,
+  },
+  {
+    path: routes.DASHBOARD,
+    element: <Auth><Layout /></Auth>,
+    children: [
+      {
+        path: routes.DASHBOARD,
+        element: <Dashboard />
+      },
+      {
+        path: routes.ALLTABLES,
+        element: <AllTables />
+      },
+      {
+        path: routes.INSIGHTS,
+        element: <Insights />
+      },
+      {
+        path: routes.PRICING,
+        element: <Pricing />
+      }
+    ]
+  }
+]);
+
 
 function App() {
 
