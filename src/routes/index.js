2b21d73e2336439ba18fb02dc0f605f7b7d51423
sheikh295 <sheikh295@users.noreply.github.com>
2023-09-20 22:20:17 +0500

    .

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+export const routes ={
+    LOGIN: '/',
+    SIGNUP: '/signup',
+    ONBOARDING: '/onboarding',
+    DASHBOARD: '/home',
+    ALLTABLES: 'all-tables',
+    INSIGHTS: 'insights',
+    PRICING: 'pricing'
+}
