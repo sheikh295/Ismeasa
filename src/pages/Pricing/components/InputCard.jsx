95ba8e7cd9d7aceb495877b90dc4b854cebd91fb
sheikh295 <sheikh295@users.noreply.github.com>
2023-09-20 23:19:16 +0500

    .

--- src/pages/Pricing/components/InputCard.jsx
+++ src/pages/Pricing/components/InputCard.jsx
@@ -1,14 +1,18 @@
+import classNames from 'classnames'
 import React from 'react'
 
 export default function InputCard() {
+
+  const cardBtn = classNames('w-[100%] bg-[#FDDB00] m-auto mt-6 rounded-[8px] justify-center flex px-[13px] py-[13px] font-[inter] font-semibold text-[#222] cursor-pointer text-[14px]')
+
   return (
     <div>
         <div className='w-[500px] h-[300px] bg-white rounded-lg shadow-2xl mt-5 ml-40'>
             <div className='w-[90%] m-auto pt-10 font-[inter]'>
-                <p className='text-[#1f1f1f] text-[14px] font-semibold'>Number of tables</p>
-                <input type='text' className='w-[100%] gap-2 px-2 py-3 items-center rounded-md border border-[#D2D2D2] mt-3' />
+                <p className='text-[#1f1f1f] text-[14px] font-semibold mb-5'>Number of tables</p>
+                <input type='number' className='w-[100%] gap-2 px-2 py-3 items-center rounded-md border border-[#D2D2D2]' />
                 <p className='text-[#101828] text-[16px] font-semibold mt-7'>Total: €5/month</p>
-                <button className='w-[100%] bg-[#FDDB00] m-auto mt-6 rounded-[8px] justify-center flex px-[13px] py-[13px] font-[inter] font-semibold text-[#222] cursor-pointer text-[14px]'>Update</button>
+                <button className={cardBtn}>Update</button>
             </div>
         </div>
     </div>
--- src/pages/Pricing/components/PricingPlan.jsx
+++ src/pages/Pricing/components/PricingPlan.jsx
@@ -1,7 +1,14 @@
+import classNames from 'classnames'
 import React from 'react'
 import { BsFillCheckCircleFill } from 'react-icons/bs'
 
 export default function PricingPlan() {
+
+  const bulletPoints = (num) => classNames('text-[#667085] text-[14px] flex items-center gap-3', {
+    'mt-8' : num == '1',
+    'mt-3' : num == '2',
+  });
+
   return (
     <div>
         <div className='w-[400px] h-[420px] bg-white rounded-lg shadow-2xl'>
@@ -9,9 +16,9 @@ export default function PricingPlan() {
                 <p className='text-[#101828] text-[40px] font-semibold'>€1/per chip</p>
                 <p className='text-[#101828] text-[16px] font-semibold mt-5'>Basic Plan</p>
                 <p className='text-[#667085] text-[14px] mt-2'>As you add more tables you will be charged €1 per month per table</p>
-                <p className='text-[#667085] text-[14px] flex items-center gap-3 mt-8'><BsFillCheckCircleFill color='#ecd96c' />Integrated Mobile app to manage NFC chips</p>
-                <p className='text-[#667085] text-[14px] flex items-center gap-3 mt-3'><BsFillCheckCircleFill color='#ecd96c' />Admin Dashboard Access</p>
-                <p className='text-[#667085] text-[14px] flex items-center gap-3 mt-3'><BsFillCheckCircleFill color='#ecd96c' />Detailed Insights about usage per table</p>
+                <p className={bulletPoints('1')}><BsFillCheckCircleFill color='#ecd96c' />Integrated Mobile app to manage NFC chips</p>
+                <p className={bulletPoints('2')}><BsFillCheckCircleFill color='#ecd96c' />Admin Dashboard Access</p>
+                <p className={bulletPoints('2')}><BsFillCheckCircleFill color='#ecd96c' />Detailed Insights about usage per table</p>
             </div>
         </div>
     </div>
