1746e0e7eeb19423f66c69d914ff3560548b0fcb
sheikh295 <mmamoorali295@gmail.com>
2023-09-25 14:03:46 +0500

    .

--- src/Root.jsx
+++ src/Root.jsx
@@ -7,9 +7,17 @@ import { LuLayoutDashboard } from 'react-icons/lu'
 import { MdContentCopy } from 'react-icons/md'
 import { FiSettings } from 'react-icons/fi'
 import { Outlet, useNavigate } from 'react-router-dom';
-
+import {
+  Dropdown,
+  Ripple,
+  initTE,
+} from "tw-elements";
 
 function Root() {
+  
+  useEffect(() => {
+    initTE({ Dropdown, Ripple });
+  }, [])
   
   const [activeTab, setActiveTab] = useState('Dashboard');
   const navigate = useNavigate();
@@ -46,7 +54,40 @@ function Root() {
         </div>
         <div className={navbarContainerStyles}>
             <div className='text-[35px] font-[inter] font-medium'>{activeTab}</div>
-            <div className='text-[18px] flex gap-3'><p className='mr-5 mt-2 cursor-pointer text-[14px]' onClick={() => {navigate('/'); sessionStorage.removeItem('token')}}>Logout</p><img src={adminImg} /><p className='mt-2'>Administrator</p> <RiArrowDropDownLine size={28} className='mt-2' /></div>
+            <div className='text-[18px] flex'>
+              <img src={adminImg} />
+              <div class="relative" data-te-dropdown-ref>
+                <button
+                  class="flex items-center whitespace-nowrap rounded bg-transparent px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal transition duration-150 ease-in-out outline-none motion-reduce:transition-none"
+                  type="button" id="dropdownMenuButton1" data-te-dropdown-toggle-ref aria-expanded="false" data-te-ripple-init data-te-ripple-color="light">
+                  Administrator
+                  <span class="ml-2 w-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      class="h-5 w-5">
+                      <path
+                        fill-rule="evenodd"
+                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                        clip-rule="evenodd" />
+                    </svg>
+                  </span>
+                </button>
+                <ul
+                  class="absolute z-[1000] float-right m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-right text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
+                  aria-labelledby="dropdownMenuButton1"
+                  data-te-dropdown-menu-ref>
+                  <li>
+                    <a
+                      onClick={() => {navigate('/'); sessionStorage.removeItem('token')}}
+                      class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                      href="#"
+                      data-te-dropdown-item-ref>Logout</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
         </div>
         <div className={outletContainer}><Outlet /></div>
     </div>
