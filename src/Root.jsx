71ad3068bb3556a4c205a39f79579494487f1576
sheikh295 <mmamoorali295@gmail.com>
2023-09-26 13:11:07 +0500

    .

--- src/Root.jsx
+++ src/Root.jsx
@@ -82,7 +82,7 @@ function Root() {
                     <a
                       onClick={() => {navigate('/'); sessionStorage.removeItem('token')}}
                       class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                      href="#"
+                      href=""
                       data-te-dropdown-item-ref>Logout</a>
                   </li>
                 </ul>
